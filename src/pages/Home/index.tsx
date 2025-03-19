447368168da9e28eccb9dcc4c434f6f16b35ee58
Alexsandro Pereira <alexpereirasilv4@gmail.com>
2025-03-19 01:46:53 -0300

    adding google maps to home screen

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,25 +1,35 @@
-import {
-  Button,
-  StyleSheet,
-  Text,
-  View,
-
-} from "react-native";
+import { Button, ScrollView, StyleSheet, Text, View } from "react-native";
 
 import { SafeAreaView } from "react-native-safe-area-context";
-
-
+import MapView from "react-native-maps";
 import React from "react";
 
 export default function Home() {
-
   return (
-    <SafeAreaView style={Style.container}> 
-
-    <View style={Style.tabsHeader}>
-
-    </View>
-      <Text>home</Text>
+    <SafeAreaView style={Style.container}>
+      <View>
+        <View style={Style.nav}></View>
+        <View style={Style.userImg}></View>
+      </View>
+      <ScrollView style={{ flex: 1 }}>
+        <MapView
+          style={{ height: 400 }}
+          zoomControlEnabled
+          zoomEnabled
+          initialRegion={{
+            latitude: -23.6071,
+            longitude: -46.752,
+            latitudeDelta: 0.01, // Ajuste para o nível de zoom desejado
+            longitudeDelta: 0.02,
+          }}
+        />
+        <View>
+          <Text>Hair salon images</Text>
+        </View>
+        <View>
+          <Text>Hair salon iformations</Text>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -30,6 +40,5 @@ const Style = StyleSheet.create({
   },
   tabsHeader: {
     flexDirection: "row",
-    
-  }
-})
+  },
+});
