68ecd354338cd30af85a9f1d87bf0e817a38d25b
Alexsandro Pereira <alexpereirasilv4@gmail.com>
2025-04-05 22:54:19 -0300

    feat: add 3 services checkboxes to services screen

--- src/Screens/Servicos/index.tsx
+++ src/Screens/Servicos/index.tsx
@@ -1,11 +1,28 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Servicos() {
+  const services: string[] = ["degrade e bigode", "degrade"];
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{flex: 1}}>
+      
       <Text>Serviços</Text>
+      <View>
+        {services.map((service) => (
+          <View style={{ flexDirection: "row", gap: 10 }}>
+            <TouchableOpacity
+              style={{
+                borderRadius: 8,
+                height: 25,
+                width: 25,
+                borderWidth: 1,
+              }}></TouchableOpacity>
+            <Text>{service}</Text>
+          </View>
+        ))}
+      </View>
     </SafeAreaView>
   );
 }
