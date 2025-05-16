aa58faaa0cbbf4c4e8d292a09b94a816dee162b3
Alexsandro P. Silva <alexpereirasilv4@gmail.com>
2025-05-16 02:37:25 -0300

    fix: ajusta posição botão cadastro
    Quando clicava no input para digitar o botão de cadastrar ficava sobre os inputs, dificultando a interação com o input e visualização

--- src/Screens/Cadastro/index.tsx
+++ src/Screens/Cadastro/index.tsx
@@ -1,4 +1,12 @@
-import { Text, View, Image, Button } from "react-native";
+import {
+  Text,
+  View,
+  Image,
+  Button,
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
+} from "react-native";
 
 import { Style } from "./styles";
 
@@ -8,24 +16,42 @@ import React from "react";
 import { Styles } from "../login/styles";
 export default function Cadastro() {
   return (
-    <View style={Style.container}>
-      <View style={Styles.headerC}>
-        <Image
-          source={require("../../assets/logoBarberLogin.png")}
-          style={Styles.headerLogo}
-        />
-      </View>
-      <View style={Styles.formsArea}>
-      <Input firstIcon icon1="user" placeholder="Nome completo"/>
-      <Input firstIcon icon1="mail" placeholder="Email" />
-      <Input firstIcon icon1="smartphone" placeholder="Phone" />
-      <Input firstIcon icon1="lock" placeholder="Password" secondIcon type="password" />
-      <Input firstIcon icon1="lock" placeholder="Password" secondIcon type="password"/>
-      </View>
-    
-      <View style={Style.button}>
-        <ButtonMain pageNav="Login" title="Cadastrar-se" buttonBG />
-      </View>
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{ flex: 1 }}>
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View style={Style.container}>
+          <View style={Styles.headerC}>
+            <Image
+              source={require("../../assets/logoBarberLogin.png")}
+              style={Styles.headerLogo}
+            />
+          </View>
+          <View style={Styles.formsArea}>
+            <Input firstIcon icon1="user" placeholder="Nome completo" />
+            <Input firstIcon icon1="mail" placeholder="Email" />
+            <Input firstIcon icon1="smartphone" placeholder="Phone" />
+            <Input
+              firstIcon
+              icon1="lock"
+              placeholder="Password"
+              secondIcon
+              type="password"
+            />
+            <Input
+              firstIcon
+              icon1="lock"
+              placeholder="Password"
+              secondIcon
+              type="password"
+            />
+          </View>
+
+          <View style={Style.button}>
+            <ButtonMain pageNav="Login" title="Cadastrar-se" buttonBG />
+          </View>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
